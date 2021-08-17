722ddd94b0255931a89c85cca49435e0a36b1501
kek <astanaevv@bk.ru>
2021-08-17 18:47:51 +0700

    Добавьте файлы проекта.

---
+++ Lesson 14.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+#include <iomanip>
+#include <stdint.h>
+
+int main()
+{
+	std::cout << "Enter word or words:" << "\n ";
+	std::string word;
+	std::cin >> word;
+	std::cout << "Your word:" << " " << word << "\n";
+	std::cout << "Number of letters:" << " " << word.length() << "\n";
+	std::cout << "First letter:" << " " << word.front() << " " << " Last letter" << " " << word.back();
+
+
+}
